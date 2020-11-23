7fda85b253a4e9fefe174ad721f45c779d1e0b3e
Arham Abiyan <arham.abiyan@gmail.com>
2020-11-23 11:34:27 +0700

    add FindByID

--- repository/mysql/mysql.go
+++ repository/mysql/mysql.go
@@ -1,8 +1,11 @@
 package mysql
 
 import (
+	"context"
 	"database/sql"
+	"time"
 
+	"github.com/arham09/go-test-sql/model"
 	repo "github.com/arham09/go-test-sql/repository"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -31,4 +34,18 @@ func NewRepository(dialect string, dsn string, idleConn int, maxConn int) (repo.
 
 func (r *repository) Close() {
 	r.db.Close()
+}
+
+func (r *repository) FindByID(id string) (*model.UserModel, error) {
+	user := new(model.UserModel)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	err := r.db.QueryRowContext(ctx, "SELECT id, name, email, phone FROM users WHERE id = $1", id).Scan(&user.ID, &user.Name, &user.Email, &user.Phone)
+	if err != nil {
+		return nil, err
+	}
+	return user, nil
 }
--- repository/repository.go
+++ repository/repository.go
@@ -1,6 +1,8 @@
 package repository
 
+import "github.com/arham09/go-test-sql/model"
+
 type Repository interface {
 	Close()
-	// FindByID(id string) (*model.UserModel, error)
+	FindByID(id string) (*model.UserModel, error)
 }
